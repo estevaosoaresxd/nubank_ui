8e8bc2cd70519d0e5dbe4eb0aeb249270b653a91
Estevao4innov <estevao.silva@4innovation.co>
2022-08-13 23:25:08 -0300

    init project

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nubank/utils/colors_standard.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: _appBar(),
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            children: const [
+              Text("A"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  PreferredSize _appBar() {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(
+        0,
+      ),
+      child: AppBar(
+        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+      ),
+    );
+  }
+}
